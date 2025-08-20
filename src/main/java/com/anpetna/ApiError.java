9ad03749ee9d3dcfdc76cf71a4b08a551ff39791
leejaeny95@gmail.com <leejaeny95@gmail.com>
2025-08-20 11:52:33 +0900

    공통사용 클래스

---
+++ src/main/java/com/anpetna/ApiError.java
@@ -0,0 +1,22 @@
+package com.anpetna;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
+
+@Data
+@JsonPropertyOrder({ "isSuccess", "resCode", "resMessage" })
+public class ApiError {
+
+    @JsonProperty("isSuccess")
+    private boolean isSuccess = false;
+
+    private int resCode;
+
+    private String resMessage;
+
+    public ApiError(int resCode, String resMessage) {
+        this.resCode = resCode;
+        this.resMessage = resMessage;
+    }
+}
---
+++ src/main/java/com/anpetna/config/RootConfig.java
@@ -0,0 +1,23 @@
+package com.anpetna.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+
+    @Bean
+    public ModelMapper getMapper() {
+
+        var modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return modelMapper;
+    }
+}
