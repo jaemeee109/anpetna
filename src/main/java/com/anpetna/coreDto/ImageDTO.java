a8a5aad1d882fe386b4260c40997b9c8435506f0
220-09\PARKHEEJIN <gomong6765@gmail.com>
2025-08-27 12:44:13 +0900

    25.08.27 다시 커밋

--- src/main/java/com/anpetna/coreDto/ImageDTO.java
+++ src/main/java/com/anpetna/coreDto/ImageDTO.java
@@ -1,5 +1,24 @@
 package com.anpetna.coreDto;
 
+import com.anpetna.coreDomain.ImageEntity;
+import lombok.*;
+
+@Getter
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImageDTO {
 
+    private Long uuid;
+    private String fileName;
+    private String url;
+    private Integer sortOrder;
+
+    public ImageDTO(ImageEntity imageEntity) {
+        this.uuid = imageEntity.getUuid();
+        this.fileName = imageEntity.getFileName();
+        this.url = imageEntity.getUrl();
+        this.sortOrder = imageEntity.getSortOrder();
+    }
 }
