c2891e1f512e37589257e44f38b0ce281d5ba738
ChaeYun430 <marchlcy@gmail.com>
2025-09-23 11:20:27 +0900

    //  코드 작성자의 의도     //  item 판매개수를 기준으로 랭킹화     //  item readAll 페이지에서는 카테고리별 랭킹 확인 가능     //  주기 설정은 메인 페이지용 현재는 searchAll의 판매량 정렬만 테스트  완료 redis 넣어놔서 연결필요

---
+++ src/main/java/com/anpetna/image/dto/ImageListDTO.java
@@ -0,0 +1,22 @@
+package com.anpetna.image.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ToString
+@Getter
+@Setter
+public class ImageListDTO {
+
+    public List<ImageDTO> images = new ArrayList<>();
+
+    public void addImage(ImageDTO imageDTO) {
+        images.add(imageDTO);
+    }
+}
