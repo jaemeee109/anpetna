88104a9ccd32ac1fc78b59edfca6ade198fb2e6c
220-11\rayoh95 <rayoh95@naver.com>
2025-10-20 15:31:32 +0900

    CICD test 2025.10.20 15:31

--- src/main/java/com/anpetna/TestController.java
+++ src/main/java/com/anpetna/TestController.java
@@ -11,7 +11,7 @@ public class TestController {
     @ResponseBody
     public String test() {
         String data = "<h1>deploy succeed</h1>";
-        data += "<h1>CICD test</h1>";
+        data += "<h1>CICD test v1.0.0</h1>";
 
         return data;
     }
